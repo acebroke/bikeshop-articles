baffb7b0fde48a713f14996070ee23f1c4e66d35
Amin Mangeot <mamindegs@hotmail.fr>
2022-10-01 14:28:44 +0200

    add change route path and other route for funny messager

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
 var express = require("express");
 var router = express.Router();
 require("../models/connection");
-var articlesModel  = require('../models/article');
+var articlesModel = require("../models/article");
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.json({ message: "Welcome my friend 🚀" });
 });
 
-router.post("/", function (req, res, next) {
-
-  articlesModel
-  .find()
-  .then(data => res.json({articles : data}))
+router.get("/articles", function (req, res, next) {
+  articlesModel.find().then((data) => res.json({ articles: data }));
 });
 
 module.exports = router;
