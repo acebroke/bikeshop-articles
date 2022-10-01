f95a2d413ef17b2568393e5d1c7d917738394f3e
Amin Mangeot <mamindegs@hotmail.fr>
2022-10-01 17:55:30 +0200

    add bikes doc webservice

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,6 +5,11 @@ sr.reveal('.getArticles',{
     distance : '50px',
     origin : 'left'
 })
+sr.reveal('.ArticleBike',{
+    interval: 300,
+    distance : '50px',
+    origin : 'top'
+})
 var options = {
     strings: ['WEBSERVICE FOR BIKERS','FROM LA CAPSULE 🚀'],
     typeSpeed: 40
